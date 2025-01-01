66d69c8fc6902a8d2118cc3f73bd9487de897ed8
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-01-01 22:02:58 +0530

    increse the pre loader time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import Header from './components/header.js';
-import Home from './pages/home.js';
-import Footer from './components/Footer.js';  // Importing Footer component
+import "./App.css";
+import Header from "./components/header.js";
+import Home from "./pages/home.js";
+import Footer from "./components/Footer.js"; // Importing Footer component
 
 import React, { useState, useEffect } from "react";
-import Preloader from "./components/PreLoader";  // Import the Preloader component
+import Preloader from "./components/PreLoader"; // Import the Preloader component
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -13,20 +13,20 @@ function App() {
     // Simulate a loading delay
     const timer = setTimeout(() => {
       setLoading(false); // Set loading to false after 3 seconds (adjust as needed)
-    }, 3000); // Adjust time as needed (in milliseconds)
+    }, 3500); // Adjust time as needed (in milliseconds)
 
-    return () => clearTimeout(timer);  // Cleanup timer on component unmount
+    return () => clearTimeout(timer); // Cleanup timer on component unmount
   }, []);
 
   return (
     <div>
       {loading ? (
-        <Preloader />  // Show preloader while loading is true
+        <Preloader /> // Show preloader while loading is true
       ) : (
         <>
           <Header />
           <Home />
-          <Footer />  {/* Adding the Footer component here */}
+          <Footer /> {/* Adding the Footer component here */}
         </>
       )}
     </div>
